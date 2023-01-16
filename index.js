7f51a910b155467c93984d6385e7a2380aeb381b
lexusegorov <alexey.egorov.job@gmail.com>
2023-01-16 20:32:22 +0700

    feat: create app

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('Hello, world!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listen port: ${port}`))
